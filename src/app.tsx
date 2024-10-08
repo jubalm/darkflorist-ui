9bb3185756e20b0db36a07aa56ec73ff785c300d
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2024-10-08 13:29:25 +0800

    rework for click handling

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,5 @@
 import './app.css'
-import { AddressCard } from './components/AddressCard'
+import { InlineCard, InlineCardSelectValue } from './components/AddressCard'
 import { Resizer } from './components/Resizer'
 import { Blockie } from './components/SVGBlockie'
 import { generateEthereumAddress } from './utils/ethereum'
@@ -9,35 +9,51 @@ export function App() {
   const address = generateEthereumAddress()
   const addressBigInt = BigInt(address)
 
-  const addressProps:Parameters<typeof AddressCard>[0] = { 
+  const addressProps:Parameters<typeof InlineCard>[0] = { 
     label: address,
-    icon: () => <Blockie address={addressBigInt} />
-   }
+    icon: () => <Blockie address={addressBigInt} />,
+    onSelect: console.log
+  }
+
+
+  const handleAddressClick = (returnValue: InlineCardSelectValue) => {
+    if (returnValue.action === 'copy') {
+      alert(`Copied ${returnValue.value} to clipboard.`)
+      return
+    }
+
+    confirm('Are you sure you want to update?')
+  }
+
 
   return (
     <div>
+
       <p style={{ color: '#aaa', marginBottom: '0.5em' }}>Address inline with text</p>
       <Resizer>
-        <p>Example address inline with text <AddressCard {...addressProps} onClick={alert} /> within a paragraph.</p>
+        <p>Example address inline with text <InlineCard {...addressProps} onSelect={handleAddressClick} /> within a paragraph.</p>
       </Resizer>
 
       <p style={{ color: '#aaa', marginBottom: '0.5em', marginTop: '2em' }}>Example with constrained widths</p>
       <Resizer>
         <p>
-          <AddressCard label='lll' icon={addressProps.icon} onClick={alert} />{ " " }
-          <AddressCard label='ETH' icon={addressProps.icon} onClick={alert} />{ " " }
-          <AddressCard label='USDT' icon={addressProps.icon} onClick={alert} />{ " " }
+          <InlineCard label='lll' icon={addressProps.icon} onSelect={handleAddressClick} />{ " " }
+          <InlineCard label='ETH' icon={addressProps.icon} onSelect={handleAddressClick} />{ " " }
+          <InlineCard label='USDT' icon={addressProps.icon} onSelect={handleAddressClick} />{ " " }
         </p>
       </Resizer>
 
       <p style={{ color: '#aaa', marginBottom: '0.5em', marginTop: '2em' }}>Vertical spacing when wrapped</p>
       <Resizer style={{ maxWidth: '50ch' }}>
         <p>
-          <AddressCard {...addressProps} onClick={alert} />
-          <AddressCard {...addressProps} onClick={alert} />
-          <AddressCard {...addressProps} onClick={alert} />
+          <InlineCard {...addressProps} onSelect={handleAddressClick} />
+          <InlineCard {...addressProps} onSelect={handleAddressClick} />
+          <InlineCard {...addressProps} onSelect={handleAddressClick} />
         </p>
       </Resizer>
+
+      <p style={{ color: 'orange', fontWeight: 'bold', marginTop: '2em' }}>Extra</p>
+      <p style={{ color: '#aaa', marginBottom: '0.5em' }}><span style={{ color: 'orange' }}>+</span> Copy and Edit buttons are tab accessible.</p>
     </div>
   )
 }
