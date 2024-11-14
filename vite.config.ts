5c537da2b6efb68d4be5675998c0017f59b6a147
Jubal Mabaquiao <jubal.mabaquiao@gmail.com>
2024-11-14 09:49:12 +0800

    fix nested css

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
+import postcssNesting from 'postcss-nesting'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [postcssNesting]
+    }
+  },
   plugins: [preact()],
   resolve: {
     alias: {
